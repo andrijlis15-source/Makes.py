974e34f58eca0dd2c051287a73ff8897235351f5
andrijlis15-source <andrijlis15@gmail.com>
2025-09-26 13:01:42 +0300

    Update оп3.py

--- оп3.py
+++ оп3.py
@@ -1,36 +1,35 @@
 students = []
 
 while True:
-    name = input("Enter student name: ")
+    name = input("Введіть ім'я студента: ")
     if name == "stop":
         break
-    grade = input("Enter student grade: ")
+    grade = input("Введіть оцінку студента: ")
     if grade == "stop":
         break
 
     try:
        grade = int(grade)
     except ValueError:
-       print("Grade must be the number!")
+       print("Оцінка повинна бути числом!")
        continue
 
     students.append({"name": name, "grade": grade})
 
-print("List of students:")
+print("Список студентів:")
 for student in students:
     print(f"{student['name']} - {student['grade']}")
 
-
 avg = sum(student["grade"] for student in students) / len(students)
 
-print(f"Average grade: {avg}")
+print(f"Середня оцінка: {avg}")
 
 excellent = []
 for student in students:
     if student['grade'] >=10 and student['grade'] <=12:
        excellent.append(student)
 
-print(f"Excellent students: {len(excellent)}")
+print(f"Відмінники: {len(excellent)}")
 for student in excellent:
     print(f"{student['name']} - {student['grade']}")
 
@@ -39,26 +38,24 @@ for student in students:
     if student['grade'] >= 7 and student['grade'] <= 9:
         well.append(student)
 
-print(f"Well students: {len(well)}")
+print(f"Добрі студенти: {len(well)}")
 for student in well:
     print(f"{student['name']} - {student['grade']}")
-
 
 notbad = []
 for student in students:
     if student['grade'] >=4 and student['grade'] <=6:
        notbad.append(student)
 
-print(f"Notbad students: {len(notbad)}")
+print(f"Середні студенти: {len(notbad)}")
 for student in notbad:
     print(f"{student['name']} - {student['grade']}")
-
 
 unsubmitted = []
 for student in students:
     if student['grade'] >=1 and student['grade'] <=3:
        unsubmitted.append(student)
 
-print(f"Unsubmitted students: {len(unsubmitted)}")
+print(f"Студенти з низькими оцінками: {len(unsubmitted)}")
 for student in unsubmitted:
     print(f"{student['name']} - {student['grade']}")
